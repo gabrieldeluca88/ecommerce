a2d464bc51d05fff011289d68eb322df60c54e97
gabrieldeluca88 <gabrieldeluca88@gmail.com>
2022-08-07 12:57:53 -0300

    sincronizarCounter

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,13 +1,19 @@
 import './ItemDetail.css';
 import ItemCount from '../ItemCount/ItemCount';
+import { useState } from 'react';
+import {Link} from 'react-router-dom'
 
 
-
-const onAdd = (count) => {
-    alert (`se agrego ${count} al carrito`);
-}; 
 
 const ItemDetail = ({product}) => {
+
+    const [quantity, setQuantity] = useState(0)
+
+    const onAdd = (count) => {
+    alert (`se agrego ${count} al carrito`);
+    setQuantity(count)
+    }; 
+
     return(
         <div className='item_card'>
             <div className='img_detail'>   
@@ -19,7 +25,7 @@ const ItemDetail = ({product}) => {
                 <h3>Precio: {product.price}</h3>
                 <h3>Stock: {product.stock}</h3>
             <div className='detailCount'>
-            <ItemCount stock={product.stock} initial={1} onAdd={onAdd}/>
+            {quantity > 0 ? <Link to='/cart'>Ir al carrito</Link> : <ItemCount stock={product.stock} initial={1} onAdd={onAdd}/>}
             </div>
                 <p>{product.description}</p>
             </div>
