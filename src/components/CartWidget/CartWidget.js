a8a6a8bddaabd8ccdd707c73570604b8ad753459
gabrieldeluca88 <gabrieldeluca88@gmail.com>
2022-08-16 20:23:40 -0300

    modificoCartWidget

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -6,7 +6,13 @@ const CartWidget = () => {
     const { getQuantity } = useContext(CartContext)
 
     const quantity = getQuantity()
-
+    if (quantity === 0){
+        return (
+            <Link to='/cart' className="CartWidget">
+                <img src='/images/cart.svg' alt='cart' className='CartImg'/>
+            </Link>
+        );
+    }
 
     return(
         <Link to='/cart' className="CartWidget">
