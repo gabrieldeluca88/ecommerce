0b09909576789b9d0e5b2312504d6f4dd7f79347
gabrieldeluca88 <gabrieldeluca88@gmail.com>
2022-09-06 19:29:37 -0300

    .env1

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,18 +2,12 @@ import { initializeApp } from "firebase/app";
 import {getFirestore} from 'firebase/firestore'
 
 const firebaseConfig = {
-  /* apiKey: process.env.REACT_APP_apiKey,
+  apiKey: process.env.REACT_APP_apiKey,
   authDomain: process.env.REACT_APP_authDomain,
   projectId: process.env.REACT_APP_projectId,
   storageBucket:process.env.REACT_APP_storageBucket,
   messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId */ 
-  apiKey: "AIzaSyBJDmAOhWHBEf8zt8Tn91bIsW9A-q9n6vo",
-  authDomain: "ecommerceback-e6507.firebaseapp.com",
-  projectId: "ecommerceback-e6507",
-  storageBucket: "ecommerceback-e6507.appspot.com",
-  messagingSenderId: "204345074785",
-  appId: "1:204345074785:web:66adb519e7cf9b1c333620"
+  appId: process.env.REACT_APP_appId 
 };
 
 
